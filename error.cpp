4622eb9c92c83cfeb7539069c294d4cd56f4f352
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-01-02 20:56:18 -0600

    Moved error logging

--- error.cpp
+++ error.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "error.h"
+
+std::unique_ptr<ExprAST> logError(const char* str) {
+    fprintf(stderr, "Error: %s\n", str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> logErrorP(const char* str) {
+    logError(str);
+    return nullptr;
+}
+
+llvm::Value* logErrorV(const char* str) {
+    logError(str);
+    return nullptr;
+}
--- error.h
+++ error.h
@@ -4,5 +4,12 @@
 
 #ifndef KALIEDOSCOPE_ERROR_H
 #define KALIEDOSCOPE_ERROR_H
+#include "llvm/IR/Verifier.h"
+#include "./AST Nodes/ExprAST.h"
+#include "./AST Nodes//PrototypeAST.h"
+
+std::unique_ptr<ExprAST> logError(const char* str);
+std::unique_ptr<PrototypeAST> logErrorP(const char* str);
+llvm::Value* logErrorV(const char* str);
 
 #endif //KALIEDOSCOPE_ERROR_H
