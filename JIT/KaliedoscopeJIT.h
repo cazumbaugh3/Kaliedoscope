e0d2a93c726b5d5e0960b0424724c51a32f046fb
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-01-08 16:08:01 -0600

    Added method declarations for JIT

--- JIT/KaliedoscopeJIT.h
+++ JIT/KaliedoscopeJIT.h
@@ -29,7 +29,33 @@ private:
     llvm::orc::RTDyldObjectLinkingLayer objectLayer;
     llvm::orc::IRCompileLayer compileLayer;
     llvm::orc::JITDylib &mainJD;
-};
+
+public:
+    KaliedoscopeJIT(std::unique_ptr<llvm::orc::ExecutionSession> ES, llvm::orc::JITTargetMachineBuilder JTMB,
+                    llvm::DataLayout dataLayout): ES(std::move(ES)), DL(std::move(DL)), mangle(*this->ES, this->DL),
+                                                  objectLayer(*this->ES, []() {
+                                                      return std::make_unique<llvm::SectionMemoryManager>();
+                                                  }),
+                                                  compileLayer(*this->ES, objectLayer,
+                                                               std::make_unique<llvm::orc::ConcurrentIRCompiler>(std::move(JTMB))),
+                                                               mainJD(this->ES->createBareJITDylib("<main>")) {
+        mainJD.addGenerator(llvm::cantFail(llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
+                DL.getGlobalPrefix()
+                )));
+        if (JTMB.getTargetTriple().isOSBinFormatCOFF()) {
+            objectLayer.setOverrideObjectFlagsWithResponsibilityFlags(true);
+            objectLayer.setAutoClaimResponsibilityForObjectSymbols(true);
+        }
+    }
+
+    ~KaliedoscopeJIT();
+
+    static llvm::Expected<std::unique_ptr<KaliedoscopeJIT>> create();
+    const llvm::DataLayout& getDataLayout();
+    llvm::orc::JITDylib& getMainJITDylib();
+    llvm::Error addModule(llvm::orc::ThreadSafeModule TSM, llvm::orc::ResourceTrackerSP RT = nullptr);
+}
+;
 
 
 #endif //KALIEDOSCOPE_KALIEDOSCOPEJIT_H
