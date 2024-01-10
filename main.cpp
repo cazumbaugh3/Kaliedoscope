636a92da0d445c549ddab141a6c3b7fa9d768198
cazumbaugh3 <czumbaugh@platinumdeveloping.net>
2024-01-09 22:11:02 -0600

    Started on adding JIT and LLVM optimizations

--- main.cpp
+++ main.cpp
@@ -12,18 +12,69 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
+#include "JIT/KaliedoscopeJIT.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/StandardInstrumentations.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Scalar/Reassociate.h"
+#include "llvm/Transforms/Scalar/SimplifyCFG.h"
+#include "error/ExitOnError.h"
+#include "LLVMPasses/passAndAnalysisManagers.h"
 
 std::unique_ptr<llvm::LLVMContext> theContext;
 std::unique_ptr<llvm::Module> theModule;
 std::unique_ptr<llvm::IRBuilder<>> builder;
 std::map<std::string, llvm::Value*> namedValues;
 
-static void initializeModule() {
+// Pass and analysis managers
+
+std::unique_ptr<llvm::FunctionPassManager>  theFPM;
+std::unique_ptr<llvm::LoopAnalysisManager> theLAM;
+std::unique_ptr<llvm::FunctionAnalysisManager> theFAM;
+std::unique_ptr<llvm::CGSCCAnalysisManager> theCGAM;
+std::unique_ptr<llvm::ModuleAnalysisManager> theMAM;
+std::unique_ptr<llvm::PassInstrumentationCallbacks> thePIC;
+std::unique_ptr<llvm::StandardInstrumentations> theSI;
+std::map<std::string, std::unique_ptr<PrototypeAST>> functionProtos;
+llvm::ExitOnError exitOnError;
+std::unique_ptr<llvm::orc::KaleidoscopeJIT> theJIT;
+
+static void initializeModuleAndManagers() {
     theContext = std::make_unique<llvm::LLVMContext>();
     theModule = std::make_unique<llvm::Module>("my cool jit", *theContext);
+    theModule->setDataLayout(theJIT->getDataLayout());
     builder = std::make_unique<llvm::IRBuilder<>>(*theContext);
-}
 
+    // Create new pass and analysis managers
+    theFPM = std::make_unique<llvm::FunctionPassManager>();
+    theLAM = std::make_unique<llvm::LoopAnalysisManager>();
+    theFAM = std::make_unique<llvm::FunctionAnalysisManager>();
+    theCGAM = std::make_unique<llvm::CGSCCAnalysisManager>();
+    theMAM = std::make_unique<llvm::ModuleAnalysisManager>();
+    thePIC = std::make_unique<llvm::PassInstrumentationCallbacks>();
+    theSI = std::make_unique<llvm::StandardInstrumentations>(*theContext, true);
+    theSI->registerCallbacks(*thePIC, theMAM.get());
+
+    // Add LLVM transform passes
+    // Do simple "peephole" optimizations and bit-twiddling optimizations.
+    theFPM->addPass(llvm::InstCombinePass());
+    // Reassociate expressions
+    theFPM->addPass(llvm::ReassociatePass());
+    // Eliminate common subexpressions
+    theFPM->addPass(llvm::GVNPass());
+    // Simplify the control flow graph (deletes unreachable nodes, etc.)
+    theFPM->addPass(llvm::SimplifyCFGPass());
+
+    // Register analysis passes used in these transform passes
+    llvm::PassBuilder PB;
+    PB.registerModuleAnalyses(*theMAM);
+    PB.registerFunctionAnalyses(*theFAM);
+    PB.crossRegisterProxies(*theLAM, *theFAM, *theCGAM, *theMAM);
+}
 
 void handleDefinition() {
     if (auto fnAST = parseDefinition()) {
@@ -91,6 +142,9 @@ void mainLoop() {
 }
 
 int main() {
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmParser();
+    llvm::InitializeNativeTargetAsmPrinter();
     binopPrecedence['<'] = 10;
     binopPrecedence['+'] = 20;
     binopPrecedence['-'] = 30;
@@ -99,7 +153,8 @@ int main() {
     fprintf(stderr, "ready> ");
     getNextTok();
 
-    initializeModule();
+    theJIT = exitOnError(llvm::orc::KaleidoscopeJIT::Create());
+    initializeModuleAndManagers();
 
     mainLoop();
 
